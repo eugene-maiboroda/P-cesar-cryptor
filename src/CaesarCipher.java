0d6e02de3675e0a04bd7c077a65f7e0770a3006c
Yevgeniy Mayboroda <javadeveloper.eugene@gmail.com>
2022-05-16 21:49:25 +0200

    Сreated methods decrypt

--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -8,8 +8,8 @@ import java.nio.file.Path;
 
 public class CaesarCipher {
 
-    private static final String ALPHABET_LOWER_CASE = "abcdefghijklmnopqrstuvwxyz.,\"\":-!? ";
-    private static final String ALPHABET_UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWSYZ.,\"\":-!? ";
+    private static final String ALPHABET_LOWER_CASE = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя.,\"\":-!? ";
+    private static final String ALPHABET_UPPER_CASE = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ.,\"\":-!? ";
 
     public CaesarCipher() {
     }
@@ -17,25 +17,59 @@ public class CaesarCipher {
     public static void encrypt(String pathToFile, int key) {
         if (key > 0) {
             try (FileReader reader = new FileReader(pathToFile);
-                 FileWriter writer = new FileWriter(String.valueOf(Files.createFile(Path.of("C:\\Users\\JEKA\\IdeaProjects\\Cryptographic calculator\\EncryptedText"))))) {
+                 FileWriter writer = new FileWriter(String.valueOf(Files.createFile(Path.of("C:\\Users\\JEKA\\IdeaProjects\\Cryptographic calculator\\Encrypted_file"))))) {
 
                 while (reader.ready()) {
-                    char reaOneSymbol = (char) reader.read();
-                    writer.write(encryptSymbolRightShift(reaOneSymbol, key));
+                    char readOneSymbol = (char) reader.read();
+                    writer.write(encryptSymbolRightShift(readOneSymbol, key));
                 }
             } catch (IOException ioException) {
                 ioException.printStackTrace();
             }
         } else {
-            System.out.println("Error");
+            System.out.println("Error - key must be greater than zero");
         }
     }
+
+    public static void decrypt (String pathToFile, int key) {
+        if (key > 0) {
+            try (FileReader reader = new FileReader(pathToFile)) {
+//                 FileWriter writer = new FileWriter(String.valueOf(Files.createFile(Path.of("C:\\Users\\JEKA\\IdeaProjects\\Cryptographic calculator\\Decrypted_file"))))) {
+
+                while (reader.ready()) {
+                    char readOneSymbol = (char) reader.read();
+//                    writer.write(decryptSymbolRightShift(readOneSymbol, key));
+                    System.out.print(decryptSymbolRightShift(readOneSymbol,key));
+                }
+//
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Error - key must be greater than zero");
+        }
+
+    }
+
     static char encryptSymbolRightShift (char symbol, int shift) {
         if (ALPHABET_UPPER_CASE.indexOf(symbol) != -1 && Character.isUpperCase(symbol)) {
             return ALPHABET_UPPER_CASE.charAt((ALPHABET_UPPER_CASE.indexOf(symbol) + shift) % ALPHABET_UPPER_CASE.length());
 
-        } else if (ALPHABET_LOWER_CASE.indexOf(symbol) != -1 && Character.isLowerCase(symbol)) {
+        } else if (ALPHABET_LOWER_CASE.indexOf(symbol) != -1 && !Character.isUpperCase(symbol)) {
             return ALPHABET_LOWER_CASE.charAt((ALPHABET_LOWER_CASE.indexOf(symbol) + shift) % ALPHABET_LOWER_CASE.length());
+
+        } else {
+            return symbol;
+        }
+    }
+    static char decryptSymbolRightShift (char symbol, int shift) {
+        if (ALPHABET_UPPER_CASE.indexOf(symbol) != -1 && Character.isUpperCase(symbol)) {
+            return ALPHABET_UPPER_CASE.charAt((ALPHABET_UPPER_CASE.indexOf(symbol) - shift + ALPHABET_UPPER_CASE.length()) % ALPHABET_UPPER_CASE.length());
+
+        } else if (ALPHABET_LOWER_CASE.indexOf(symbol) != -1 && !Character.isUpperCase(symbol)) {
+            return ALPHABET_LOWER_CASE.charAt((ALPHABET_LOWER_CASE.indexOf(symbol) - shift + ALPHABET_LOWER_CASE.length()) % ALPHABET_LOWER_CASE.length());
 
         } else {
             return symbol;
